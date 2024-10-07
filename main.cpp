c0ee320d7ee068317c13f06969e19ceef92aad72
LufunoMphagi <115547653+LufunoMphagi@users.noreply.github.com>
2024-10-07 07:48:41 +0200

    Update main.cpp, added more testings

--- main.cpp
+++ main.cpp
@@ -43,10 +43,57 @@ int main()
     cout << "Front Door Lock Status: " << frontDoorLock.getStatus()  << endl;
 
     // Test Thermostat
+    cout << "\nTesting Thermostat" << endl;
+    thermostat.setTemperature(21.5);
+    cout << "Thermostat Set Temperature: " << thermostat.getTemperature() << "°C" << endl;
+
+    cout << "\nCreating Rooms and Sections" << endl;
+    Room livingRoom("Living Room");
+    livingRoom.addDevice(&livingRoomLight);
+    livingRoom.addDevice(&thermostat);
+
+    Section house("House");
+    house.addRoom(&livingRoom);
+
+    cout << "Devices in " << livingRoom.getName() << "" << endl;
+    for(auto deviceCount : livingRoom.getDevices())
+    {
+        deviceCount->getStatus();
+    }
 
     // Test Macro Command to turn off all lights
+    cout << "\nTesting MacroRoutine to Turn Off All Lights" << endl;
+    TurnOffAllLights execute;
+    MacroRoutine routine;
+    routine.execute();
+
+    cout << "Living Room Light Status after Macro: " << livingRoomLight.getStatus()  << endl;
+    cout << "Kitchen Light Status after Macro: " << kitchenLight.getStatus() << endl;
+
+    // Test Observer pattern with sensors and alarms
+    cout << "\nTesting Sensor and Alarm (Observer pattern)" << endl;
+    Sensor motionSensor;
+    Alarm houseAlarm;
+    motionSensor.addDevice(&houseAlarm);  
+    motionSensor.detectMotion();      
+
+    // Test Smart Thermostat Integration (Strategy Pattern)
+    cout << "\nTesting SmartThermostatIntegrator using ownedd thermostat" << endl;
+    SmartThermostatIntegrator integrator;
+    integrator.setTemperature(20);  
     
 
+    cout << "Current temperature: " << integrator.getTemperature() << "°C" << endl;
+    cout << "Device type: " << integrator.getDeviceType() << endl;
+    cout << "Status: " << integrator.getStatus() << endl;     
+
+    cout << "\nTesting SmartThermostatIntegrator using external thermostat" << endl;
+    OldThermostat externalThermostat;
+    SmartThermostatIntegrator integratorWithExternal(&externalThermostat);
+    integratorWithExternal.setTemperature(25);  
+
+    cout << "External Thermostat Temperature: " << integratorWithExternal.getTemperature() << "°C" << endl;
+    cout << "Status: " << integratorWithExternal.getStatus() << endl;
 
     return 0;
 }
